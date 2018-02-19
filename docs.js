ffe8437f1763003b98c1e08fc2346ec7e0999d52
OG Novuh <jamooney2000@gmail.com>
2018-02-18 19:17:10 -0500

    Update docs.

--- docs.js
+++ docs.js
@@ -1,40 +1,6 @@
 const Docma = require('docma');
 const Package = require('./package');
 
-/*
-const config = {
-  src: [
-    './src/*.js',
-    './README.md'
-  ],
-  dest: './docs',
-  template: {
-    options: {
-      title: Package.name,
-      navItems: [
-        {
-          label: 'Documentation',
-          href: '?api=r6-discord',
-          iconClass: 'ico-book'
-        },
-        {
-          label: 'GitHub',
-          href: Package.homepage,
-          target: '_blank',
-          iconClass: 'ico-md ico-github'
-        }
-      ]
-    }
-  }
-};
-
-Docma.create()
-  .build(config)
-  .catch(error => {
-    console.log(error);
-  });
-*/
-
 Docma.create()
   .build({
     app: {
