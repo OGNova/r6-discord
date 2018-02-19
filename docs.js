55439cc4d169b58bc95996a2027ede1a6b866298
OG Novuh <jamooney2000@gmail.com>
2018-02-18 19:25:32 -0500

    Update to docs. Please work.

--- docs.js
+++ docs.js
@@ -1,51 +1,39 @@
 const Docma = require('docma');
 const Package = require('./package');
 
-Docma.create()
-  .build({
-    app: {
+
+const config = {
+  src: [
+    './src/*.js',
+    './README.md'
+  ],
+  dest: './docs',
+  template: {
+    options: {
       title: Package.name,
-      base: '/r6-discord',
-      entrance: 'content:readme',
-      routing: 'query',
-      server: Docma.ServerType.GITHUB
-    },
-    markdown: {
-      gfm: true,
-      tables: true,
-      breaks: true,
-      pedantic: true,
-      sanitize: true,
-      smartLists: true,
-      smartypants: true,
-      tasks: true,
-      emoji: true
-    },
-    src: [
-      { r6discord: './src/*.js' },
-      { readme: './README.md' }
-    ],
-    dest: './docs',
-    template: {
-      options: {
-        title: Package.name,
-        navItems: [
-          {
-            label: 'Readme',
-            href: '?content=readme'
-          },
-          {
-            label: 'Documentation',
-            href: '?api=r6-discord',
-            iconClass: 'ico-book'
-          },
-          {
-            label: 'GitHub',
-            href: Package.homepage,
-            target: '_blank',
-            iconClass: 'ico-md ico-github'
-          }
-        ]
-      }
-    } 
+      navItems: [
+        {
+          label: 'Readme',
+          href: '?content=readme'
+        },
+        {
+          label: 'Documentation',
+          href: '?api=r6-discord',
+          iconClass: 'ico-book'
+        },
+        {
+          label: 'GitHub',
+          href: Package.homepage,
+          target: '_blank',
+          iconClass: 'ico-md ico-github'
+        }
+      ]
+    }
+  }
+};
+
+Docma.create()
+  .build(config)
+  .catch(error => {
+    console.log(error);
   });
