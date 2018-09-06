f00d8ad6e8065c286c87d7c642bbc69b73450a2e
OGNova <jamooney2000@gmail.com>
2018-09-05 20:39:04 -0400

    Attempted fixes at NPM not installing the package.

---
+++ index.js
@@ -0,0 +1,53 @@
+const fetch = require('node-fetch');
+
+module.exports = class R6 {
+  constructor() {}
+
+  stats(username, platform, operators) {
+    return new Promise((resolve, reject) => {
+      const platforms = ['uplay', 'xone', 'ps4'];
+      if (!platforms.includes(platform)) return reject(new TypeError('Platform must be one of [uplay | xone | ps4]'));
+      
+      if (!username || typeof username !== 'string') return reject(new TypeError('Invalid username. The username must be a string.'));
+      
+      operator = operators || false;
+      
+      if (typeof operator !== boolean) return reject(new TypeError('The operators value must be a boolean'));
+      if (typeof platforms !== 'string' || !platform) return reject(new TypeError('Invalid platform. Platform must be one of [uplay | xone | ps4]'));
+
+      let endpoint = `https://api.r6stats.com/api/v1/players/${username.toString()}/?platform=${platform}`;
+      if (operators) {
+        endpoint = `https://api.r6stats.com/api/v1/players/${username}/operators/?platform=${platform}`;
+      }
+
+      fetch(endpoint, (error, body) => {
+        if (!error) {
+          return resolve(JSON.parse(body));
+        } else {
+          return reject(JSON.parse(body));
+        }
+      });
+    });
+  }
+
+  profile(username, platform) {
+    return new Promise((resolve, reject) => {
+      const platforms = ['uplay', 'xone', 'ps4'];
+      if (!platforms.includes(platform)) return reject(new TypeError('Platform must be one of [uplay | xone | ps4]'));
+
+      if (!username || typeof username !== 'string') return reject(new TypeError('Invalid username. The username must be a string.'));
+
+      if (typeof platforms !== 'string' || !platform) return reject(new TypeError('Invalid platform. Platform must be one of [uplay | xone | ps4]'));
+
+      const endpoint = `https://api.r6stats.com/api/v1/users/${username}/profile/?platform=${platform}`;
+
+      fetch(endpoint, (error, body) => {
+        if (!error) {
+          return resolve(JSON.parse(body));
+        } else {
+          return reject(JSON.parse(body));
+        }
+      });
+    });
+  }
+};
